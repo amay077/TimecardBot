363d3fc5bc390bb73b592b5796b039581fe472ea
amay077 <okuokuoku@gmail.com>
2017-08-05 14:04:04 +0900

    Add extension

---
+++ TimecardLogic/Extensions/UserExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimecardLogic.DataModels;
+
+namespace TimecardLogic.Extensions
+{
+    public static class UserExtensions
+    {
+        public static DateTime NowAsUserTz(this User self)
+        {
+            var nowUtc = DateTime.UtcNow;
+            var tzUser = TimeZoneInfo.FindSystemTimeZoneById(self.TimeZoneId);
+            return TimeZoneInfo.ConvertTimeFromUtc(nowUtc, tzUser); // ユーザーのタイムゾーンでの現在時刻
+        }
+    }
+}
